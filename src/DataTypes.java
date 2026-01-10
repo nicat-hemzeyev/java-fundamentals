b8d4010cbe2b0e7d73072d3f9af0de78fab35daf
nicat-hemzeyev <hemzeyevnicat25@gmail.com>
2026-01-10 23:05:27 +0400

    Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,50 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = a * b;
+        System.out.println(c);
+        int g = a + b;
+        System.out.println(g);
+        int m = a / b;
+        System.out.println(m);
+        int n = a - b;
+        System.out.println(n);
+        double x = (double) 18.0F;
+        double y = (double) 5.0F;
+        double bolme = x / y;
+        System.out.println(bolme);
+        char h = 'a';
+        System.out.println(h);
+        String soz = "quba almasi";
+        System.out.println(soz);
+        boolean cavab = false;
+        System.out.println(cavab);
+        boolean cavab2 = true;
+        System.out.println(cavab2);
+        boolean muq = false;
+        System.out.println(muq);
+        boolean muq2 = true;
+        System.out.println(muq2);
+        double d = (double) 5.0F;
+        ++d;
+        System.out.println("d1=" + d);
+        d += (double) 5.0F;
+        System.out.println("d2=" + d);
+        double q = (double) 6.0F;
+        --q;
+        System.out.println("q1=" + q);
+        double z = (double) 6.0F;
+        --z;
+        System.out.println("z1=" + z);
+        double t = (double) 54.0F;
+        double qaliq = t % (double) 10.0F;
+        System.out.println("qaliq=" + qaliq);
+        int tam = (int) t / 10;
+        System.out.println("tam=" + tam);
+        double tam1 = t / (double) 10.0F;
+        int tam2 = (int) tam1;
+        System.out.println(tam2);
+    }
+}
+
