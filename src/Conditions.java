7a662c80e31c156c6003749ece7ebc98e741c562
nicat-hemzeyev <hemzeyevnicat25@gmail.com>
2026-01-11 23:26:33 +0400

    Add Conditions class with if/else and switch examples

---
+++ src/Conditions.java
@@ -0,0 +1,41 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int number = 5;
+        // if-else nümunəsi
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+        // switch nümunəsi
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+
